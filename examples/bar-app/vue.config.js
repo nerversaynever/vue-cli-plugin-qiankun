2a13502c6ae047238ae801082b55b80d8eaacc00
zhangxiuling <zhangxiuling@huixiangtech.cn>
2022-10-19 18:28:54 +0800

    修复在主应用访问子应用报错，在子应用配置后，需要重启项目
    //通过fetch访问子项目，需要子项目进行可跨域配置，vue.config.js：
    	devServer: {
    		headers: {
    			'Access-Control-Allow-Origin': '*'
    		}
    	}

--- examples/bar-app/vue.config.js
+++ examples/bar-app/vue.config.js
@@ -3,5 +3,7 @@ module.exports = {
   indexPath: process.env.NODE_ENV === 'production'
     ? 'source.html'
     : 'index.html',
-  publicPath: process.env.VUE_APP_BASE_PATH
+  publicPath: process.env.VUE_APP_BASE_PATH,
+	//通过fetch访问子项目，需要子项目进行可跨域配置，vue.config.js：
+	devServer: { headers: { 'Access-Control-Allow-Origin': '*' } }
 }
--- examples/foo-app/vue.config.js
+++ examples/foo-app/vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  productionSourceMap: false,
-  indexPath: process.env.NODE_ENV === 'production'
-    ? 'source.html'
-    : 'index.html',
-  publicPath: process.env.VUE_APP_BASE_PATH
+	productionSourceMap: false,
+	indexPath: process.env.NODE_ENV === 'production' ?
+		'source.html' :
+		'index.html',
+	publicPath: process.env.VUE_APP_BASE_PATH,
+	//通过fetch访问子项目，需要子项目进行可跨域配置，vue.config.js：
+	devServer: {
+		headers: {
+			'Access-Control-Allow-Origin': '*'
+		}
+	}
 }
